3c58efa4acc99e970550597a7f696c8e2691055f
Patrick Ly <112744444447551+patrickly1@users.noreply.github.com>
2023-11-02 13:43:10 -0400

    Added calculator function all of the operators and equal sign

--- javascript.js
+++ javascript.js
@@ -23,7 +23,7 @@ function operate(num1, operator, num2) {
         return subtract(num1, num2);
     }
 
-    else if (operator === "*") {
+    else if (operator === "X") {
         return multiply(num1, num2);
     }
 
@@ -37,20 +37,41 @@ const buttonElement = document.querySelectorAll(".buttons");
 const content = document.createElement("div"); 
 content.classList.add("content");
 content.textContent = "";
+let num1 = null;
+let num2 = null;
+let operator = null;
 
-buttonElement.forEach (buttonElement => {
-    buttonElement.addEventListener("click", function(event) {
-        const button = event.target;
-        
-        if (button.classList.contains("number")) {
-         content.textContent += button.textContent; 
+buttonElement.forEach (button => {
+    button.addEventListener("click", function(event) {
+        const buttonText = event.target;
+        if (buttonText.classList.contains("number")) {
+            if (operator === null) {
+                num1 = (num1 === null) ?  buttonText.textContent : num1 + buttonText.textContent;
+            }   else {
+                num2 = (num2 === null) ? buttonText.textContent : num2 + buttonText.textContent;
+            }
+            content.textContent += buttonText.textContent; 
         }
-        else if (button.classList.contains("operator")) {
-         content.textContent += button.textContent; 
+        else if (buttonText.classList.contains("operator")) {
+            operator = buttonText.textContent;
+            content.textContent += buttonText.textContent;
+        }   
+        else if (buttonText.classList.contains("equals")) {
+            if (num1 !== null && num2 !== null && operator !== null) {
+                const result = operate(parseFloat(num1), operator, parseFloat(num2));
+                content.textContent = result;
+                num1 = result;
+                num2 = null;
+                operator = null;
+            }            
         }
-     
-     });
-})
+        else if (buttonText.classList.contains("clear")) {
+            num1 = null;
+            operator = null;
+            num2 = null;
+            content.textContent = null;
+        }
+    });
+});
 
-
-displayElement.appendChild(content);
+displayElement.appendChild(content)
